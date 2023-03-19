f7f8ccd4a9bfac27ee68a390acc6dacc1b565abe
Alexander Candia <tony_john_08@hotmail.com>
2023-03-19 14:16:32 -0500

    agregando src

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'header': '#002A5E',
+        'primary': '#FFFFFF',
+        'card': '#F7F7F7',
+        'subtext': '#02026a',
+      },
+      fontFamily: {
+        'montserrat':"'Montserrat', serif",
+        'poppins': "'Poppins', monospace",
+      },
+    },
+  },
+  plugins: [
+    
+  ],
+}
